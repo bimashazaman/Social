c605979b389d8003492465f8ff57591fe64f3930
bimashazaman <bimashazaman1234.com>
2023-03-31 07:20:59 +0600

    Post index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::latest()->paginate(50);
         return view('posts.index', compact('posts'));
     }
 
