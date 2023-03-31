b8cff479a4ddc9f11f08d937e73fb334180c985c
bimashazaman <bimashazaman1234.com>
2023-03-31 06:57:29 +0600

    Base

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts.index');
 });
 
 Auth::routes();
